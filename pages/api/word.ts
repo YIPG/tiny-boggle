df40a29e3cb493c8a8381fef11f53966b8e8284f
yipg <around.mr.child@gmail.com>
2020-05-27 10:59:54 +0900

    eneble file load in api route

--- pages/api/word.ts
+++ pages/api/word.ts
@@ -2,9 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 import fs from "fs"
 import path from "path"
+import getConfig from "next/config"
 import { Trie } from "../../utils/trie"
 
-const dictionaryPath = path.join(process.cwd(), "assets/dictionary.json")
+const { serverRuntimeConfig } = getConfig()
+
+//
+const dictionaryPath = path.join(
+  serverRuntimeConfig.PROJECT_ROOT,
+  "./assets/dictionary.json"
+)
 
 const fileName = fs.readFileSync(dictionaryPath, "utf-8")
 const allWords: string[] = JSON.parse(fileName)
@@ -16,6 +23,5 @@ allWords.forEach((word) => {
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const word: string = req.body
   const ok = trie.contain(word)
-  console.log(ok)
   res.send(ok ? word : "0")
 }
