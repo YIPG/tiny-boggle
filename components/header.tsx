ed23ac8a3b2c8099dbe2143bdd25008a8f8cab1c
yipg <around.mr.child@gmail.com>
2020-07-04 23:32:21 +0900

    chore: change twitter card type to large image sum

--- components/header.tsx
+++ components/header.tsx
@@ -23,7 +23,7 @@ export default function Header({
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image} />
       <meta property="og:site_name" content={title} />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@yuyaito_hoge" />
       <meta name="twitter:url" content={image} />
       <meta name="twitter:title" content={title} />
